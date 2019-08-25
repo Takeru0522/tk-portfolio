ab62d2572a62b3020361aa39e0781a4e995f910a
TAKERU <takeru.kayamori@gmail.com>
2019-08-25 20:09:27 +0900

    minimal viable

---
+++ public/main.js
@@ -0,0 +1,34 @@
+console.log('Hello World');
+
+// const test = document.querySelector('.test');
+// console.log(test.innerHTML);
+
+// let num = 1
+// console.log(num)
+
+// function func(){
+//     num = 2
+//     console.log(num)
+//     test.innerHTML = 'changed'
+// }
+
+// test.addEventListener('show', func)
+
+// const element =  document.querySelector('.my-element')
+// element.classList.add('animated', 'zoomIn', )
+
+
+// function play(){
+//     animeSample.classList.add('play-animation');
+//     animeSample.classList.remove('pause-animation');
+// }
+// function pause(){
+//     animeSample.classList.remove('play-animation');
+//     animeSample.classList.add('pause-animation');
+// }
+// const animeSample = document.querySelector('.anime-sample');
+// const playBtn = document.querySelector('.play-btn');
+// const pauseBtn = document.querySelector('.pause-btn');
+
+// playBtn.addEventListener('click', play)
+// pauseBtn.addEventListener('click', pause)
