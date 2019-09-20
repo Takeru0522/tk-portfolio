c932229c296d6abfd3fb28637eb054a741069a72
TAKERU <takeru.kayamori@gmail.com>
2019-09-20 21:21:55 +0900

    change some layout

--- public/main.js
+++ public/main.js
@@ -1,5 +1,85 @@
 console.log('Hello World');
 
+
+// Nav
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+
+
+
+
+const nav1 = document.getElementById("nav-1"); // Home
+const nav2 = document.getElementById("nav-2"); // About
+const nav3 = document.getElementById("nav-3"); // Work
+const nav4 = document.getElementById("nav-4"); // Skill
+const nav5 = document.getElementById("nav-5"); // Contact
+
+// const header = document.getElementById("header");
+const header = document.querySelector('header')
+const sectionA = document.getElementById("section-a"); // About
+const sectionB = document.getElementById("section-b");
+const sectionC = document.getElementById("section-c");
+const sectionD = document.getElementById("section-d");
+
+
+nav1.addEventListener('click', () => {
+    
+    sectionA.scrollIntoView({
+        behavior: "smooth", 
+        block: "start", 
+        inline: "nearest"
+    });
+})
+nav2.addEventListener('click', () => {
+    sectionA.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+nav3.addEventListener('click', () => {
+    sectionB.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+nav4.addEventListener('click', () => {
+    sectionC.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+nav5.addEventListener('click', () => {
+    sectionD.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+
+
 // const test = document.querySelector('.test');
 // console.log(test.innerHTML);
 
